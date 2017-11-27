cd346ede83d59e51b554f179350ab0b165ffbcfa
8NW <awoodard19@kentdenver.org>
2017-11-27 09:41:30 -0700

    Comments for First Application video

--- 01-first-app/server.js
+++ 01-first-app/server.js
@@ -1,12 +1,13 @@
-var http = require('http');
+var http = require('http'); //gives access to http functions such as create server
 
-function onRequest(request, response) {
+function onRequest(request, response) { // this is so the server does something when accessed
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello World');
-    response.end();
+    response.end(); // just shows that the request is done
 }
 
-http.createServer(onRequest).listen(8000);
+http.createServer(onRequest).listen(8000); //creates the server and listens for any requests on port 8000
 
 /* Notes: 
-- 
+- look above
+*/
