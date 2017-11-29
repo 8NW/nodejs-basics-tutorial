7121a536c417e8dab375ac42ba35bde0f69873df
8NW <awoodard19@kentdenver.org>
2017-11-28 20:36:11 -0700

    video 6 notes

--- 03-node-only-render-html/server.js
+++ 03-node-only-render-html/server.js
@@ -1,16 +1,16 @@
 var http = require('http');
-var fs = require('fs');
+var fs = require('fs'); //imports the file system task allows for reading of files
 
 function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null, function(error, data) {
-        if (error) {
-            response.writeHead(404);
+    response.writeHead(200, {'Content-Type': 'text/html'}); // the / html allows it to be a html file instead of a plain type text
+    fs.readFile('./index.html', null, function(error, data) { // this reads the file
+        if (error) { // just in case of an error
+            response.writeHead(404); //error type
             response.write('File not found!');
         } else {
-            response.write(data);
+            response.write(data); // this passes the data into it
         }
-        response.end();
+        response.end(); // once again ends the function 
     });
 }
 
