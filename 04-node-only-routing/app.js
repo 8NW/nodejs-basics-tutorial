0a09c0be5a3bef88534e61046735a6929d48ea2e
8NW <awoodard19@kentdenver.org>
2017-11-28 21:14:23 -0700

    video 6 notes

--- 04-node-only-routing/app.js
+++ 04-node-only-routing/app.js
@@ -13,19 +13,19 @@ function renderHTML(path, response) {
     });
 }
 
-module.exports = {
+module.exports = { //defines what objects should be exported
   handleRequest: function(request, response) {
       response.writeHead(200, {'Content-Type': 'text/html'});
 
-      var path = url.parse(request.url).pathname;
-      switch (path) {
-          case '/':
+      var path = url.parse(request.url).pathname; // pathname is the /_____
+      switch (path) { // this check what route was taken
+          case '/': //default type leads to home/ index page
               renderHTML('./index.html', response);
               break;
-          case '/login':
+          case '/login': // causes the login page to load instead
               renderHTML('./login.html', response);
               break;
-          default:
+          default: //if neither are triggered it defaults to the error message
               response.writeHead(404);
               response.write('Route not defined');
               response.end();
