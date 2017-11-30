ad1d9d35ad59df4841a9837a12d2b396185470e7
8NW <awoodard19@kentdenver.org>
2017-11-30 14:30:42 -0700

    Something?

--- 06-handlebars/app.js
+++ 06-handlebars/app.js
@@ -4,16 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var hbs = require('express-handlebars');
+var hbs = require('express-handlebars'); // the req for the handlebars
 
 var routes = require('./routes/index');
 
 var app = express();
 
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'})); //shows that the engine is using hbs
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs'); // no longer goes to jade and install links to the handlebars
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
